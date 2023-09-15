fd0a9399f5ab73125501d2dc2b5ff67be760ef7d
csiformk <formk@kermed.fr>
2023-09-15 12:01:41 +0200

    Les variables

--- app/index.php
+++ app/index.php
@@ -9,9 +9,78 @@ plusieurs
 lignes
 */
 
-echo "Test php";
+//echo "Test php";
 ?>
 
-<h1>php</h1>
+<!-- <h1>php</h1> -->
 
 <?php
+
+// Variables
+ $variable = 'Une variable';
+ $Variable = 'Une autre variable';
+
+//  echo $variable;
+//  echo '<br>';
+//  echo $Variable;
+
+//$nb = '5';
+//$nb = 5;
+//$nb = (string) '5';
+//$nb = (int) '5';
+//$nb = (float) '5.6';
+// $nb = (bool) 'true';
+
+//echo $nb;
+//printf($nb);
+// var_dump($nb);
+
+// $camelCase = 'une variable en camel case';
+// $snake_case = 'une variable en snake case';
+// //$kebab-case = 'une variable en kebab case';
+// // kebab case pas valid en php
+// $PascalCase = 'une variable en pascale case';
+
+// $fruit1 = 'pomme';
+// $fruit2 = 'banane';
+
+// echo "$fruit1" . " " . "$fruit2";
+// echo "<p>$fruit1" . " " . "$fruit2</p>";
+// echo "<p>$fruit1" . "<br> " . "$fruit2</p>";
+
+// $fruit1 = 'pomme';
+// $fruit1 = 'banane';
+// $fruit1 = 'kiwi';
+
+// echo $fruit1;
+
+// $resultat = '';
+// $resultat .= '<h1>';
+// $resultat .= 'Le fruit choisi est :';
+// $resultat .= '</h1>';
+// $resultat .= ' la banane';
+
+// echo $resultat;
+
+// $titre = "Les fruits";
+// $texte ="Lorem ipsum dolor sit, amet consectetur";
+?>
+
+<!-- <h2><?php echo $titre ?></h2>
+<h2><?= $titre ?></h2>
+
+<p>
+    <?= $texte ?>;
+</p> -->
+
+<?php
+
+$data1 = "Hello";
+$data2 = "planete";
+$data3 = "Mars";
+$data4 = "les terriens";
+
+// Afficher la phrase suivante dans un paragraphe
+// Hello, les terriens.Je viens de la planete Mars.
+
+
