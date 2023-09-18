1c7f52415fc443cbf7b4de245cbaf1def5ba3151
csiformk <formk@kermed.fr>
2023-09-18 10:35:07 +0200

    Condition if/else/elseif

--- app/structure-controle/index.php
+++ app/structure-controle/index.php
@@ -1,3 +1,30 @@
 <?php
+/* if/else/elseif
 
-echo "Test";
+>
+<
+>=
+<=
+==
+===
+!=
+!==
+
+*/
+
+// $age = 10;
+// if($age >= 18) {
+//     echo "Vous avez le droit de voter.";
+// } else {
+//     echo "Vous n'avez pas le droit de voter !";
+// }
+
+$heure = 06;
+// Afficher "On est le matin" lorsque l'heure est inferieur à 12h00 
+//et "On est le soir" lorsque l'heure est supérieur à 18h00
+
+if ($heure <= 12) {
+    echo "On est le matin.";
+} elseif ($heure >= 18) {
+    echo "On est le soir.";
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 [Site offciel](https://www.php.net/)
 
 [Documentation php fr](https://www.php.net/manual/fr/)
+
+[Les structures de contrôle](https://www.php.net/manual/fr/language.control-structures.php)
