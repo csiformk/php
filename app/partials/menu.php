bb8fef6719ed81c5011ba4475ea10ddbc124d5f9
csiformk <formk@kermed.fr>
2023-09-18 09:50:29 +0200

    Reorganbistation des dossiers

--- app/partials/menu.php
+++ app/partials/menu.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="">Home</a></li>
-    <li><a href="">Structures de controles</a></li>
+    <li><a href="../structure-controle/">Structures de controles</a></li>
     <li><a href="">Boucles</a></li>
     <li><a href="">Tableaux</a></li>
     <li><a href="">Fonctions</a></li>
