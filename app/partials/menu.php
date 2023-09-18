ca0100dbc582c62008570f69b9a91f3762e345af
csiformk <formk@kermed.fr>
2023-09-18 12:12:10 +0200

    Ajout tableaux

--- app/partials/menu.php
+++ app/partials/menu.php
@@ -1,7 +1,7 @@
 <ul>
     <li><a href="">Home</a></li>
     <li><a href="../structure-controle/">Structures de controles</a></li>
+    <li><a href="../tableaux/">Tableaux</a></li>
     <li><a href="../boucles/">Boucles</a></li>
-    <li><a href="">Tableaux</a></li>
     <li><a href="">Fonctions</a></li>
 </ul>
