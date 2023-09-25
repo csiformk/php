e1af2e7eb3b2c601f747741a5421f0051dc44210
csiformk <formk@kermed.fr>
2023-09-25 15:04:01 +0200

    Fonction utilisateur

--- app/partials/menu.php
+++ app/partials/menu.php
@@ -3,5 +3,5 @@
     <li><a href="../structure-controle/">Structures de controles</a></li>
     <li><a href="../tableaux/">Tableaux</a></li>
     <li><a href="../boucles/">Boucles</a></li>
-    <li><a href="">Fonctions</a></li>
+    <li><a href="../fonctions/">Fonctions</a></li>
 </ul>
