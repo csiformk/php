494d8bd45b6e6d95a7b11e94fe564ccbff67056e
csiformk <formk@kermed.fr>
2023-09-29 14:19:28 +0200

    Refonte des dossiers

--- app/index.php
+++ app/index.php
@@ -3,15 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inclusion</title>
+    <title>Formulaire</title>
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
 <?php include('./partials/header.php'); ?>
 <main>
     <section>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquid ducimus consequatur quasi veritatis at vitae? Magnam sed, animi doloribus, assumenda facilis sequi repellendus quam molestias culpa, hic consequatur! Vitae!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquid ducimus consequatur quasi veritatis at vitae? Magnam sed, animi doloribus, assumenda facilis sequi repellendus quam molestias culpa, hic consequatur! Vitae!</p>
+        <h1>Formulaire</h1>
+        <form action="" method="">
+            
+        </form>
+       
     </section>
 </main>
 <?php include('./partials/footer.php'); ?>
--- app/partials/menu.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="">Home</a></li>
-    <li><a href="../structure-controle/">Structures de controles</a></li>
-    <li><a href="../tableaux/">Tableaux</a></li>
-    <li><a href="../boucles/">Boucles</a></li>
-    <li><a href="../fonctions/">Fonctions</a></li>
-    <li><a href="../boucles-tableaux/">Parcours d'un tableau</a></li>
-</ul>
