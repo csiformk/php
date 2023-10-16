24376628e617d63db515a6833f307adfbc3275ff
csiformk <formk@kermed.fr>
2023-10-16 16:07:15 +0200

    Ajout d'une vue pour l'index

---
+++ src/Database.php
@@ -0,0 +1,28 @@
+<?php
+//https://www.php.net/manual/en/pdo.connections.php
+
+class Database
+{
+    public $connexion;
+
+    public function __construct()
+    {
+        try {
+            $this->connexion = new PDO('mysql:host=mariadb;dbname=blog',
+             'root',
+             'root',
+            [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+        );
+        } catch (PDOException $e) {
+            exit('Erreur de connexion à la base de donnée :' . $e);
+        }
+    }
+
+    public function query($query)
+    {
+        $statement = $this->connexion->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+}
+
--- src/index.php
+++ src/index.php
@@ -1,47 +1,8 @@
 <?php
 include './fonctions.php';
+require './Database.php';
 
-// class Person {
-//     //attributs (variables)
-//     public $nom;
-//     public $age;
+$db = new Database();
+$articles = $db->query('SELECT * FROM post')->fetchAll();
 
-//     //methodes (fonctions)
-//     public function courir() {
-//         echo $this->nom . ' court lentement !';
-//     }
-// }
-
-// $person1 = new Person();
-// $person1->nom = 'Jon doe';
-// $person1->age = 30;
-// $person1->courir();
-
-//https://www.php.net/manual/en/pdo.connections.php
-
-try {
-    $connexion = new PDO('mysql:host=mariadb;dbname=blog', 'root', 'root');
-} catch (PDOException $e) {
-    exit('Erreur de connexion à labase de donnée :' . $e);
-}
-
-$articles = $connexion->query('SELECT * from post')->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<ul>
-    <?php foreach($articles as $article): ?>
-        <li><?=$article['titre']?></li>
-    <?php endforeach;?>
-</ul>
-</body>
-</html>
-
-<?php $connexion = null; ?>
+include './views/index.view.php';
