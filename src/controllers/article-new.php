4fb0dbe0895f1517812a502ed4dea387ef3fc00c
csiformk <formk@kermed.fr>
2023-10-23 15:18:58 +0200

    Affichage persistant des champs en cas d'erreur

--- src/controllers/article-new.php
+++ src/controllers/article-new.php
@@ -3,25 +3,50 @@ require 'models/Database.php';
 $db = new Database;
 $heading = 'Nouvelle recette';
 
+$titre = $contenu = $errors = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST'):
+    $errors = [];
 
     $titre = cleanData($_POST['titre']);
     $contenu = cleanData($_POST['contenu']);
 
-    // if ( empty($titre) || empty($contenu) ) :
-    if ( strlen($titre) === 0 || strlen($contenu) === 0 ) :
-        echo 'Champ titre ou contenu vide !!!' ;
-        exit();
-    else:
+   /*  if ( strlen($titre) === 0 || strlen($contenu) === 0 ) :
+        $errors[] = 'Champ titre ou contenu vide !!!' ;
+    endif; */
+
+    if ( strlen($titre) === 0 ) :
+        $errors[] = 'Champ titre vide !!!' ;
+    endif;
+
+    if ( strlen($titre) >= 50 ) :
+        $errors[] = 'Champ titre depasse les 50 carcatéres !!!' ;
+    endif;
+
+    if ( strlen($contenu) === 0 ) :
+        $errors[] = 'Champ contenu vide !!!' ;
+    endif;
+
+    if ( strlen($contenu) >= 1000 ) :
+        $errors[] = 'Champ contenu depasse les 100 carcatéres !!!' ;
+    endif;
+
+
+    if ( empty($errors) ) :
         $db->query('INSERT INTO post (titre, contenu) VALUES (:titre, :contenu)' , [
             'titre' => $titre,
             'contenu' => $contenu
         ]
         );
+        header('Location: /articles');
+        exit();
     endif;
-    header('Location: /articles');
-    exit();
     
 endif;
 
-view('article-new',$heading);
+view('article-new',[
+    'heading' => $heading,
+    'titre' => $titre,
+    'contenu' => $contenu,
+    'errors' => $errors
+]);
--- src/views/article-new.view.php
+++ src/views/article-new.view.php
@@ -1,10 +1,27 @@
-<h2><?=$model?></h2>
+<h2><?=$model['heading']?></h2>
 <section>
     <form method="POST">
     <label for="titre">Titre :</label>
-    <input type="text" name="titre" id="titre">
+    <input type="text" name="titre" id="titre" 
+    <?php if ( isset( $model['titre'] ) ) : ?>
+        value="<?=$model['titre']?>"
+    <?php endif; ?>
+    >
     <label for="contenu">Recette :</label>
-    <textarea name="contenu" id="contenu" cols="30" rows="10"></textarea>
+    <textarea name="contenu" id="contenu" cols="30" rows="10">
+    <?php if ( isset( $model['contenu'] ) ) : ?>
+        <?=$model['contenu']?>
+    <?php endif; ?>
+    </textarea>
     <input type="submit" value="Ajouter">
     </form>
+        <?php
+        if ( isset($model['errors']) && !empty($model['errors']) ) :
+            foreach ( $model['errors'] as $error ):
+        ?>
+                <p class="error"><?=$error?></p>
+        <?php
+            endforeach;
+        endif;
+        ?>
 </section>
